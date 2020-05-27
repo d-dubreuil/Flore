787c36af7d164601c98cbb2711929edbe4cb4f37
DamienD <dubreuil.damien@laposte.net>
2020-05-27 15:13:34 +0200

    lien premier essai

--- flore-angular/src/app/flore/flore.component.ts
+++ flore-angular/src/app/flore/flore.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Flore} from '../model/Flore';
+import {FloreForm} from '../model/FloreForm';
+import {FloreService} from '../services/flore.service';
+import {CaracteristiqueService} from '../services/caracteristique.service';
 
 @Component({
   selector: 'app-flore',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FloreComponent implements OnInit {
 
-  constructor() { }
+  floreformulaire: FloreForm;
+  caracteristiques: Array<string>;
+  flores: Array<Flore>;
+
+  constructor(private floreService: FloreService, private caracteristiqueService: CaracteristiqueService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  search() {
+    this.floreService.findByFormulaire(this.floreformulaire).subscribe(resp => this.flores = resp, error => console.log(error));
   }
 
 }
