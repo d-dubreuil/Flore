34ede40537ebc1e1f42b5f5b2505630947f2f5b5
DamienD <dubreuil.damien@laposte.net>
2020-05-29 09:47:57 +0200

    Faune (travail sur les view)

--- src/main/java/flore/model/ReferentielFaune.java
+++ src/main/java/flore/model/ReferentielFaune.java
@@ -22,7 +22,7 @@ public class ReferentielFaune {
 	private int version;
 	@ManyToOne
 	@JoinColumn(name = "caracteristique_id")
-	@JsonView(Views.ViewReferentielFaune.class)
+	@JsonView({Views.ViewReferentielFaune.class, Views.ViewFaune.class})
 	private Caracteristique caracteristique;
 	@ManyToOne
 	@JoinColumn(name = "flore_id")
