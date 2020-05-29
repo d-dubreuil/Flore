a1adede2f0d2645d807dc8a095971d37c9a0fe7d
DamienD <dubreuil.damien@laposte.net>
2020-05-29 16:46:47 +0200

    Fiche flore + liens

--- flore-angular/src/app/fiche-flore/fiche-flore.component.ts
+++ flore-angular/src/app/fiche-flore/fiche-flore.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FloreService} from '../services/flore.service';
 import {Title} from '@angular/platform-browser';
 import {Flore} from '../model/Flore';
 import {normalizeExtraEntryPoints} from '@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs';
+import {FauneService} from '../services/faune.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-fiche-flore',
@@ -10,21 +12,21 @@ import {normalizeExtraEntryPoints} from '@angular-devkit/build-angular/src/angul
   styleUrls: ['./fiche-flore.component.scss']
 })
 export class FicheFloreComponent implements OnInit {
-  nomFlore:string;
-  flore:Flore;
+  nomFlore: string;
+  flore: Flore;
 
-  constructor(public floreService:FloreService, private titleService: Title) {
-    this.titleService.setTitle("Flore");
+  constructor(public floreService: FloreService, private titleService: Title, public fauneService: FauneService, private router: Router) {
+    this.titleService.setTitle('Fiche-Flore');
   }
 
   ngOnInit(): void {
     this.findFlore();
   }
 
-  findFlore(){
-    this.floreService.findByNom(this.nomFlore).subscribe(next=>{
-      this.flore=next;
-    },error => console.log(error));
+  findFlore() {
+    this.floreService.findByNom(this.nomFlore).subscribe(next => {
+      this.flore = next;
+    }, error => console.log(error));
   }
 
   filterCarac(flore: Flore, nomCarac: string): string {
@@ -38,6 +40,43 @@ export class FicheFloreComponent implements OnInit {
     }
 
     return 'non renseigné';
+  }
+
+  tableauCarac(flore: Flore, nomCarac: string): Array<string> {
+    let listeCarac: Array<string> = new Array<string>();
+
+    if (flore.referentielCaracteristiques) {
+
+      for (let refCarac of flore.referentielCaracteristiques) {
+        if (refCarac.caracteristique.nom == nomCarac) {
+          listeCarac.push(refCarac.caracteristique.valeur);
+        }
+      }
+    }
+    return listeCarac;
+  }
+
+  valeurCarac(flore: Flore, nomCarac: string, valeurCarac: string): boolean {
+    if (flore.referentielCaracteristiques) {
+
+      for (let refCarac of flore.referentielCaracteristiques) {
+        if (refCarac.caracteristique.nom == nomCarac && refCarac.caracteristique.valeur == valeurCarac) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  redirectToFicheFlore(nomFlore: string) {
+    this.floreService.findByNom(nomFlore).subscribe(resp => {
+      console.log(this.floreService.flore);
+      this.floreService.flore = resp[0];
+    }, error => console.log(error));
+  }
+
+  redirectToFicheFaune(nomFaune: string) {
+    this.fauneService.findByNom(nomFaune).subscribe(resp => this.fauneService.faune = resp, error => console.log(error));
   }
 
 }
