b4fd875db9a31725535399514554e9444169892c
DamienD <dubreuil.damien@laposte.net>
2020-06-02 10:09:40 +0200

    Panier

--- flore-angular/src/app/model/Paiement.ts
+++ flore-angular/src/app/model/Paiement.ts
@@ -7,18 +7,20 @@ export class Paiement {
   dtValidite:string;
   cryptogramme:string
   montant:number;
+  typeCarte:string;
   commande:Commande;
 
 
   constructor(id?: number, version?: number, numeroCarte?: string,
               dtValidite?: string, cryptogramme?: string, montant?: number,
-              commande?: Commande) {
+              typeCarte?:string, commande?: Commande) {
     this.id = id;
     this.version = version;
     this.numeroCarte = numeroCarte;
     this.dtValidite = dtValidite;
     this.cryptogramme = cryptogramme;
     this.montant = montant;
+    this.typeCarte = typeCarte;
     this.commande = commande;
   }
 }
